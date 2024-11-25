47e63bd2403c9c114b6aa844b94dc3fced1dbfd7
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-11-25 12:26:15 +0100

    add RouteNotFound component and handle 404 routes in App component

--- src/App.tsx
+++ src/App.tsx
@@ -10,12 +10,17 @@ function Home() {
   );
 }
 
+function RouteNotFound() {
+  return <div>Page not found</div>;
+}
+
 function App() {
   return (
     <div className="h-screen">
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
+          <Route path="*" element={<RouteNotFound />} />
         </Route>
       </Routes>
     </div>
