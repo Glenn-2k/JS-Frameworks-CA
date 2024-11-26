91478ffd1371b3b5b4c0777a3924fb0e750dc21c
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-11-26 23:33:36 +0100

    enhance Header component styling; add items-center class for better alignment

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,7 +3,7 @@ import Nav from "../Nav";
 
 const Header = () => {
   return (
-    <header className="bg-gray-800 sticky top-0 z-20 mx-auto w-full flex justify-between ">
+    <header className="bg-gray-800 sticky top-0 z-[20] mx-auto w-full flex justify-between items-center ">
       <Logo />
       <Nav />
       <div className="Cart">Cart here</div>
