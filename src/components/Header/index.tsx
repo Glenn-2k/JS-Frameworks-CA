8af9f2375ed62b23d86e77435b9e73fb209f5b4b
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-11-26 22:53:25 +0100

    add logo images and update Header component to display logo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
+import logo from "../../assets/logowhite.png";
+
 const Header = () => {
   return (
     <header className="bg-gray-800">
       <nav className="flex justify-between">
-        <div className="logo">Logo here</div>
+        <img src={logo} className="h-16 w-auto m-4" alt="logo image" />
         <div className="Cart">Cart here</div>
       </nav>
     </header>
