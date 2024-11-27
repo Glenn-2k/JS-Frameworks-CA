3e9091df8ceec682d5379be7198b38de343de881
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-11-27 14:26:08 +0100

    enhance useApi hook; add error handling for network responses and reset error state on new requests

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,30 +1,37 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState } from "react";
+import { set } from "react-hook-form";
 
-function useApi<T> = (url: string) => { 
-    const [data, setData] = useState<T | null>(null);
-    const [isLoading, setIsLoading] = useState(false);
-    const [isError, setIsError] = useState(false);
+function useApi<T>(url: string) {
+  const [data, setData] = useState<T | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
 
-    useEffect(() => {
-        async function getData() {
-            try {
-                setIsLoading(true);
-                const response = await fetch(url);
-                const json = await response.json();
+  useEffect(() => {
+    async function getData() {
+      try {
+        setIsLoading(true);
+        setIsError(false);
 
-                setData(json);
-            }
-            catch (error) {
-                setIsError(true);
-            }
-            finally {
-                setIsLoading(false);
-            }
+        const response = await fetch(url);
+
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
         }
-        getData();
-    }, [url]);
 
-    return { data, isLoading, isError };
-};
+        const json = await response.json();
 
-export default useApi
+        setData(json);
+      } catch (error) {
+        console.error("Error fetching data", error);
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    getData();
+  }, [url]);
+
+  return { data, isLoading, isError };
+}
+
+export default useApi;
