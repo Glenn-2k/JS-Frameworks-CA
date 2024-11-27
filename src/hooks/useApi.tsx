cdef3e26357f7a14afe79f970b9a8820cfc36c36
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-11-27 14:23:03 +0100

    implement custom useApi hook; add data fetching logic with loading and error states

--- src/hooks/useApi.tsx
+++ src/hooks/useApi.tsx
@@ -1,3 +1,30 @@
 import { useEffect, useState } from 'react';
 
-const useApi = (url: string) => { https://v2.api.noroff.dev/online-shop
+function useApi<T> = (url: string) => { 
+    const [data, setData] = useState<T | null>(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+        async function getData() {
+            try {
+                setIsLoading(true);
+                const response = await fetch(url);
+                const json = await response.json();
+
+                setData(json);
+            }
+            catch (error) {
+                setIsError(true);
+            }
+            finally {
+                setIsLoading(false);
+            }
+        }
+        getData();
+    }, [url]);
+
+    return { data, isLoading, isError };
+};
+
+export default useApi
