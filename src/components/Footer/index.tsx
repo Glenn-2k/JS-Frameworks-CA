acd660de604e9b89941d8ddca208da3c96177773
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-11-28 18:14:30 +0100

    remove fixed positioning from Footer component for improved layout

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white fixed w-full p-4 text-center bottom-0">
+    <footer className="bg-gray-800 text-white w-full p-4 text-center bottom-0">
       <p>© 2024 Glenn</p>
     </footer>
   );
