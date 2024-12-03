6206639886e58abb79bbdbfc6a3bde66fdbe9992
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-12-03 11:48:49 +0100

    refactor Footer component to remove unnecessary bottom alignment

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-gray-800 text-white w-full p-4 text-center bottom-0">
+    <footer className="bg-gray-800 text-white w-full p-4 text-center">
       <p>© 2024 Glenn</p>
     </footer>
   );
