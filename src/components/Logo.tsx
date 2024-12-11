0a28840a41e81ac261eb3fe6e625d9d042a78c4d
Glenn-2k <128032061+Glenn-2k@users.noreply.github.com>
2024-12-11 15:01:29 +0100

    feat: make logo clickable to navigate to home page

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,9 +1,16 @@
 import logoImage from "../assets/logowhite.png";
+import { useNavigate } from "react-router-dom";
 
 const Logo = () => {
+  const navigate = useNavigate();
   return (
     <div>
-      <img src={logoImage} className="h-16 w-auto m-4" alt="logo image" />
+      <img
+        src={logoImage}
+        className="h-16 w-auto m-4 cursor-pointer"
+        alt="logo image"
+        onClick={() => navigate("/")}
+      />
     </div>
   );
 };
